f41d10552505c17c0110fd8f50bbfd2cd3f99eaf
Ingrid Jufkin <Ingrid@Ingrids-MBP.lan>
2021-05-22 14:49:28 +0300

    frontend tests added

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,8 +1,7 @@
 <template>
   <div
     class="bg-white shadow rounded px-3 pt-3 pb-5 border-2"
-    :class="'border-' + task.color.toLowerCase() + '-400'"
-  >
+    :class="'border-' + task.color.toLowerCase() + '-400'">
     <div class="flex justify-between">
       <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">
         {{ task.title }}
@@ -11,9 +10,9 @@
         Delete
       </button>
     </div>
-    <div class="flex mt-4 justify-between items-center">
+    <div class="flex mt-4 justify-between items-center date">
       <span class="text-sm text-gray-600">{{
-        $moment(task.date).format("DD.MM.YYYY")
+        moment(task.date).format("DD.MM.YYYY")
       }}</span>
 
       <badge v-if="task.priority" :color="badgeColor"
@@ -28,6 +27,7 @@
 <script>
 import Badge from "./Badge.vue";
 import axios from "axios";
+import moment from 'moment';
 
 export default {
   data() {
@@ -57,9 +57,10 @@ export default {
     },
   },
 
-methods: {
+  methods: {
+    moment,
     async deleteTodo() {
-      let newName = this.$store.state.name
+      let newName = this.$store.state.name;
       //let test6 = this.task._id
       await axios({
         url: `${this.apiURL}api/deleteTask`,
@@ -67,10 +68,10 @@ methods: {
         data: this.task,
       });
       // eslint-disable-next-line no-console
-        //console.log("olen test6 " + test6),
-      this.$emit('task-deleted', {userName: newName});
+      //console.log("olen test6 " + test6),
+      this.$emit("task-deleted", { userName: newName });
+    },
   },
-},
 
   // async moveTask(event, column) {
   //   if (event.delete) {
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -10,51 +10,96 @@ describe("Badge.vue", () => {
     expect(wrapper.contains('.test')).toBe(true);
 
   });
+  //------FAKE TEST RUNS -------------
+  // it('should set correct classes for task', async () => {
 
+  //   const TaskCard = {
+  //     props: ['color'],
+  //     template: '<div>{{color}}</div>'
+  //   }
+
+  //   const wrapper = mount(TaskCard, {
+  //     propsData: {
+  //       color: 'purple',
+  //     },
+  //   })
+  //     expect(wrapper.text()).toBe('purple');
+  // });
+  //----------------------------------
+
+  //it('should set correct classes for task', async () => {
+
+  // const TaskCardNew = {
+  //   props: ['task.color'],
+  //   template: '<div>{{task.color}}</div>'
+  // }
+  //----------TEST TRIAL ---------------
+  // const wrapper = mount(TaskCard, {
+  //   propsData: {
+  //     color: '',
+  //   },
+  // })
+  //   await wrapper.setData({color: this.propsData.testcolor })
+  //     expect(wrapper.text()).toBe('purple');
+  // });
+
+  //-----------------Only Taskcard colour class controll ------------
   describe('TaskCard.vue', () => {
-//------FAKE TEST RUNS -------------
-    // it('should set correct classes for task', async () => {
+    it('checks color class', async () => {
+      const wrapper = mount(TaskCard, { //mount joooksutab task cardi etteantud andmetega läbi
+        propsData: {
+          task: {
+            color: 'RED'
+          },
+        },
+      });
+      //const color = 'GRAY';
+      //await wrapper.setProps({ task: { color } });
 
-    //   const TaskCard = {
-    //     props: ['color'],
-    //     template: '<div>{{color}}</div>'
-    //   }
+      const divs = wrapper.findAll('div'); //find all divs
+      const div = divs.at(0); // takes first div
 
-    //   const wrapper = mount(TaskCard, {
-    //     propsData: {
-    //       color: 'purple',
-    //     },
-    //   })
-    //     expect(wrapper.text()).toBe('purple');
-    // });
-//----------------------------------
-    it('should set correct classes for task', async () => {
+      expect(div.classes()).toContain('border-red-400');
+    });
+  });
+  //-----If color is changed to gray, expected new taskColour value and class to be gray ------
+  describe('TaskCard.vue', () => {
+    it('checks color class', async () => {
+      const wrapper = mount(TaskCard, { //mount runs task card with given data
+        propsData: {
+          task: {
+            color: 'RED'
+          },
+        },
+      });
+      const color = 'GRAY';
+      await wrapper.setProps({ task: { color } });
 
-      // const TaskCardNew = {
-      //   props: ['task.color'],
-      //   template: '<div>{{task.color}}</div>'
-      // }
+      expect(wrapper.vm.task.color).toContain('GRAY'); //vm  = view model - allows to access data inside wrapper
 
+      const divs = wrapper.findAll('div'); //find all divs
+      const div = divs.at(0); // takes first div
+
+      expect(div.classes()).toContain('border-gray-400');
+    });
+
+    //-------------DATE FORMAT---------------
+    it('should display date in correct format', () => {
       const wrapper = mount(TaskCard, {
         propsData: {
-          testcolor: 'purple',
+          task: {
+            date: new Date('2021-04-29T17:20:37.603+00:00'),
+            color: ''
+          },
         },
-      })
-      await wrapper.setData({color: this.propsData.testcolor })
-        expect(wrapper.text()).toBe('purple');
+      });
+      // //------------find all spans-----------------
+      // let span = wrapper.find('span');
+      // expect(span.text()).toBe('29.04.2021')
+
+      //------------find date divs that cntain spans-----------------
+      expect(wrapper.find('.date span').text()).toBe('29.04.2021');
     });
-    // set custom task object to propsData
 
-    // check if wrapper has correct color class (for example purple task has to have border-purple-400 class)
-
-  })
-
-
-
-  // it('should display date in correct format', async () => {
-
-  //   // set custom task object to propsData
-
-  //   // check if date element has correct text
-
-  })
+  });
+});
